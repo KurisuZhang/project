ca770cd17876c1eb28cd08a8f9801df40f012491
Lin <619724696@qq.com>
2024-08-06 10:18:02 -0400

    v1.1

---
+++ coupons/src/main/java/com/example/coupons/CouponsApplication.java
@@ -0,0 +1,13 @@
+package com.example.coupons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponsApplication.class, args);
+	}
+
+}
---
+++ coupons/src/test/java/com/example/coupons/CouponsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coupons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
